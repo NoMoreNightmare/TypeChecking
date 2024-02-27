7f03279254ba6d1a2c1a75b186637463240e25a4
s2517285 <s2517285@ed.ac.uk>
2024-02-27 17:38:33 +0000

    attempt to find bugs

--- choco/type_checking.py
+++ choco/type_checking.py
@@ -418,11 +418,10 @@ def check_expr(o: LocalEnvironment, r: Type, op: Operation) -> Type:
             t = str_select_rule(o, r, e1, e2)
         elif isinstance(e1, choco_ast.ExprName):
             e1_type = check_expr(o, r, e1)
-            # if e1_type == str_type:
-            #     t = str_select_rule(o, r, e1, e2)
-            # elif isinstance(e1_type, ListType):
-            #     t = list_select_rule(o, r, e1, e2)
-            t = list_select_rule(o, r, e1, e2)
+            if e1_type == str_type:
+                t = str_select_rule(o, r, e1, e2)
+            elif isinstance(e1_type, ListType):
+                t = list_select_rule(o, r, e1, e2)
         else:
             print("Semantic error:")
             exit(0)
@@ -482,10 +481,11 @@ def var_init_rule(o: LocalEnvironment, r: Type, id: str, e1: Operation):
 def var_assign_stmt_rule(o: LocalEnvironment, r: Type, id: str, e1: Operation):
     t = o[id]
     t1 = check_expr(o, r, e1)
-    if isinstance(t1, ListType):
-        check_assignment_compatibility(t1.elem_type, t)
-    else:
-        check_assignment_compatibility(t1, t)
+    # if isinstance(t1, ListType):
+    #     check_assignment_compatibility(t1.elem_type, t)
+    # else:
+    #     check_assignment_compatibility(t1, t)
+    check_assignment_compatibility(t1, t)
 
 
 # [STMT-DEF-LIST] rule
